8bba956eb0ee3d5f9224c140666e0533cc142d56
Okuhle charlieman <okuhlecharlieman@gmail.com>
2023-09-16 14:32:45 +0200

    added alt on logo

--- src/components/logo.js
+++ src/components/logo.js
@@ -3,7 +3,11 @@ import React from "react";
 function logo() {
   return (
     <div>
-      <img width="60px" src="https://i.ibb.co/Wn2MHxb/logo192.jpg"></img>
+      <img
+        width="60px"
+        src="https://i.ibb.co/Wn2MHxb/logo192.jpg"
+        alt="logo"
+      ></img>
     </div>
   );
 }
