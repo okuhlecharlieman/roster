6dee37bdd87458ac156af9c9fed6f847e4fd1b0c
Okuhle charlieman <okuhlecharlieman@gmail.com>
2023-09-16 14:16:09 +0200

    add logo and style app

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from "./navbar";
 
 const names = ["Thando", "Athabile", "Mama", "Charlie"];
 
@@ -29,10 +30,11 @@ function Home() {
 
   return (
     <div>
-      <h1>Random Names and Dates Display</h1>
-      <ul>
+      <Navbar />
+
+      <ul className="bg-slate-50 p-4 sm:px-8 sm:pt-6 sm:pb-8 lg:p-4 xl:px-8 xl:pt-6 xl:pb-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-4 text-sm leading-6">
         {repeatedNamesWithDates.map((item, index) => (
-          <li key={index}>
+          <li className="bg-slate-200" key={index}>
             {item.name} - {item.date}
           </li>
         ))}
---
+++ src/components/logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function logo() {
+  return (
+    <div>
+      <img src="https://i.ibb.co/Wn2MHxb/logo192.jpg"></img>
+    </div>
+  );
+}
+
+export default logo;
