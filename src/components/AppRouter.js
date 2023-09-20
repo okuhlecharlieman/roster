1630f73cf5130dc2cc358ea24dc5217498bb0ee5
Okuhle <okuhlecharlieman72@gmail.com>
2023-09-20 13:35:45 +0200

    added 404 page

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Admin from "./home";
 import Home from "./checkDatabase";
+import FOF from "./fof";
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-
+        <Route path="*" element={<FOF />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </Router>
