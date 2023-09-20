881ebeaa93267dfe1c88a298a19611f23d932580
Okuhle <okuhlecharlieman72@gmail.com>
2023-09-20 11:05:04 +0200

    added admin banner

--- src/components/home.js
+++ src/components/home.js
@@ -84,6 +84,9 @@ function Home() {
           </li>
         ))}
       </ul>
+      <h1 className="bg-slate-500 p-4 sm:px-8 sm:pt-6 sm:pb-8 lg:p-4 xl:px-8 xl:pt-6 xl:pb-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-4 text-sm leading-6">
+        Admin
+      </h1>
     </div>
   );
 }
