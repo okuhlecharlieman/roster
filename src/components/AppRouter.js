78c554bda4a48be7424d9d13ff826d8453a17200
Okuhle <okuhlecharlieman72@gmail.com>
2023-09-21 11:15:03 +0200

    removed 404 route

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Admin from "./home";
 import Home from "./checkDatabase";
-import FOF from "./fof";
+// import FOF from "./fof";
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="*" element={<FOF />} />
+        {/* <Route path="*" element={<FOF />} /> */}
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </Router>
